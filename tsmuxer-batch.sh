d9c639ad7f8c3d1d28f54136b4b97dc30370749d
Ian Ward <ian@excess.org>
2011-11-27 11:36:10 -0500

    make source extension a var

--- tsmuxer-batch.sh
+++ tsmuxer-batch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TSMUXER=tsMuxeR
+SOURCE_EXT=.MTS   # MS fat directory name patents caused this
 
 USAGE="Usage: $0 source-m2ts-dir dest-ts-dir"
 
@@ -14,7 +15,7 @@ fi
 cd "$1"
 SOURCE=`pwd`
 
-for fn in *.MTS; do
+for fn in "*$SOURCE_EXT"; do
 	# need a temp file because tsmuxer expects a name ending in .meta :-(
 	METAFILE=$(tempfile -s.meta)
 	trap "rm -- '$METAFILE'" EXIT
@@ -24,7 +25,7 @@ V_MPEG4/ISO/AVC, "$SOURCE/$fn", fps=29.970, insertSEI, contSPS, track=4113
 A_AC3, "$SOURCE/$fn", track=4352
 CONTENT
 
-	$TSMUXER "$METAFILE" "$2/${fn/.MTS}.ts"
+	$TSMUXER "$METAFILE" "$2/${fn/$SOURCE_EXT}.ts"
 	rm -- "$METAFILE"
 	trap - EXIT
 done
