7aa69b60820a249a6549bb76ea66881feca42e98
Ian Ward <ian@excess.org>
2011-11-27 11:26:08 -0500

    first working version

--- tsmuxer-batch.sh
+++ tsmuxer-batch.sh
@@ -15,10 +15,16 @@ cd "$1"
 SOURCE=`pwd`
 
 for fn in *.MTS; do
-	echo $TSMUXER <(cat <<METAFILE
+	# need a temp file because tsmuxer expects a name ending in .meta :-(
+	METAFILE=$(tempfile -s.meta)
+	trap "rm -- '$METAFILE'" EXIT
+	cat >"$METAFILE" <<CONTENT
 MUXOPT --no-pcr-on-video-pid --new-audio-pes --vbr  --vbv-len=500
 V_MPEG4/ISO/AVC, "$SOURCE/$fn", fps=29.970, insertSEI, contSPS, track=4113
 A_AC3, "$SOURCE/$fn", track=4352
-METAFILE) "$2/${fn/.MTS}.ts"
-	read
+CONTENT
+
+	$TSMUXER "$METAFILE" "$2/${fn/.MTS}.ts"
+	rm -- "$METAFILE"
+	trap - EXIT
 done
